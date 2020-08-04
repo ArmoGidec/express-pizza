306ed2a00339555da5fda1c0f7c787c23273d8df
diatomiccoder <chudak_93@live.com>
2020-08-04 19:47:46 +0500

    fix layout: change grid breakpoint

--- src/client/src/views/Home.vue
+++ src/client/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row>
-        <v-col v-for="pizza in pizzas" :key="pizza.id" cols="12" xs="6" md="4" lg="3">
+        <v-col v-for="pizza in pizzas" :key="pizza.id" cols="12" sm="6" md="4" lg="3">
             <Card :pizza="pizza" />
         </v-col>
     </v-row>

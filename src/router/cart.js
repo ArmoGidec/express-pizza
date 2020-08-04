7c7f02efb92079065165540a77ab45826c58832d
diatomiccoder <chudak_93@live.com>
2020-08-04 23:49:49 +0500

    fix cart functionality

--- src/router/cart.js
+++ src/router/cart.js
@@ -1,22 +1,17 @@
 const router = require('express').Router();
-const Cart = require('../models/Cart.js');
 
 router.use((req, _, next) => {
-    req.session.cart = Cart.of(req.session.cart);
+    req.session.cart = req.session.cart || [];
     next();
 });
 
 router.get('/', (req, res) => {
-    /**
-     * @type {import('../models/Cart.js')}
-     */
-    const cart = req.session.cart;
-    res.send(cart.getItems());
+    res.send(req.session.cart);
 });
 
 router.post('/set', (req, res) => {
     try {
-        req.session.cart = new Cart(req.body);
+        req.session.cart = req.body;
         res.send();
     } catch (error) {
         res.status(400).send(error);
